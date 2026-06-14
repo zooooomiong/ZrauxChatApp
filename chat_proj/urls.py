6e7ffc13eb18a5f96f14a20781a253ebc2e3b4f7
nima <ssu87556@gmail.com>
2026-06-14 22:10:35 +0330

    add static file serving for development

--- chat_proj/urls.py
+++ chat_proj/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 from django.shortcuts import render
@@ -12,3 +14,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("chat/", include("chat.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
